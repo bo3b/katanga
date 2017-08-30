52e822bc1921b92c22b3f1661655128e4ee73ff8
bo3b <github@bo3b.net>
2017-08-30 16:54:40 -0700

    Setup hook for Direct3DCreate9
    Starting hook for DX9 creation.  Creating and attaching hook seems OK.
    
    Will still crash Unity if we try to set an OnFunctionCalled routine at the app level.  Since this is not really necessary or desired, will skip this for now.

--- Assets/DrawSBS.cs
+++ Assets/DrawSBS.cs
@@ -23,7 +23,7 @@ public class DrawSBS : MonoBehaviour
         //string nativeDLLName = Environment.CurrentDirectory + @"\NativePlugin.dll";
         string game = @"G:\Games\The Ball\Binaries\Win32\theball.exe";
 
-//        noiseTex = new Texture2D(width, height, TextureFormat.ARGB32, false);
+        //        noiseTex = new Texture2D(width, height, TextureFormat.ARGB32, false);
         _spyMgr = new NktSpyMgr();
         hresult = _spyMgr.Initialize();
         if (hresult != 0)
@@ -36,25 +36,49 @@ public class DrawSBS : MonoBehaviour
         // C# app directory which can make the game crash.  This must be done before CreateProcess.
         // This also changes the working directory, which will break Deviare's ability to find
         // the NativePlugin, so we'll use full path descriptions for the DLL load.
+        // This must be reset back to the Unity game directory, otherwise Unity will
+        // crash with a fatal error.
 
         Directory.SetCurrentDirectory(Path.GetDirectoryName(game));
+        {
+            // Launch the game, but suspended, so we can hook our first call and be certain to catch it.
 
-        // Launch the game, but suspended, so we can hook our first call and be certain to catch it.
+            print("Launching: " + game + "...");
+            _gameProcess = _spyMgr.CreateProcess(game, true, out continueevent);
+            if (_gameProcess == null)
+                throw new Exception("Game launch failed.");
 
-        print("Launching: " + game + "...");
-        _gameProcess = _spyMgr.CreateProcess(game, true, out continueevent);
-        if (_gameProcess == null)
-            throw new Exception("Game launch failed.");
+            // Hook the primary DX9 creation call of Direct3DCreate9, which is a direct export of 
+            // the d3d9 DLL.  All DX9 games must call this interface.
+            // We set this to flgOnlyPostCall, because we want to use the IDirect3D9 object it returns.
 
-        // Ready to go.  Let the game startup.  When it calls Direct3DCreate9, we'll be
-        // called in the NativePlugin::OnFunctionCall
+            print("Hook the D3D9.DLL!Direct3DCreate9...");
+            NktHook d3dHook = _spyMgr.CreateHook("D3D9.DLL!Direct3DCreate9", (int)eNktHookFlags.flgOnlyPostCall);
+            if (d3dHook == null)
+                throw new Exception("Failed to hook D3D9.DLL!Direct3DCreate9");
 
-        print("Continue game launch...");
-        _spyMgr.ResumeProcess(_gameProcess, continueevent);
+            // Make sure the CustomHandler in the NativePlugin at OnFunctionCall gets called when this 
+            // object is created. At that point, the native code will take over.
+
+//            d3dHook.AddCustomHandler(nativeDLLName, 0, "");
+
+            // Finally attach and activate the hook in the still suspended game process.
+
+            d3dHook.Attach(_gameProcess, true);
+            d3dHook.Hook(true);
+
+
+            // Ready to go.  Let the game startup.  When it calls Direct3DCreate9, we'll be
+            // called in the NativePlugin::OnFunctionCall
+
+            print("Continue game launch...");
+            _spyMgr.ResumeProcess(_gameProcess, continueevent);
+        }
 
         print("Restoring Working Directory to: " + drawSBS_directory);
         Directory.SetCurrentDirectory(drawSBS_directory);
     }
+
 
     // Update is called once per frame
     void Update()
