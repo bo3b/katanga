82075f64728ae10d824838f5ad279ee5f2c68519
bo3b <github@bo3b.net>
2017-08-30 16:34:10 -0700

    Launch game, with Working Directory. Correctly save and restore WD over the game launch, so that the game sees its normal WD, and so that Unity does not see it change. Both TheBall and Limbo still work, and Unity was complaining about changing the WD.

--- Assets/DrawSBS.cs
+++ Assets/DrawSBS.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using Nektra.Deviare2;
+using System.IO;
 
 
 public class DrawSBS : MonoBehaviour
 {
     static NktSpyMgr _spyMgr;
+    static NktProcess _gameProcess;
     public int width = 1024, height = 1024;
     public Texture2D noiseTex;
 
@@ -16,6 +18,10 @@ public class DrawSBS : MonoBehaviour
     void Start()
     {
         int hresult;
+        object continueevent;
+        string drawSBS_directory = Environment.CurrentDirectory;
+        //string nativeDLLName = Environment.CurrentDirectory + @"\NativePlugin.dll";
+        string game = @"G:\Games\The Ball\Binaries\Win32\theball.exe";
 
 //        noiseTex = new Texture2D(width, height, TextureFormat.ARGB32, false);
         _spyMgr = new NktSpyMgr();
@@ -24,6 +30,30 @@ public class DrawSBS : MonoBehaviour
             throw new Exception("Deviare initialization error.");
 
         print("Successful SpyMgr Init");
+
+
+        // We must set the game directory specifically, otherwise it winds up being the 
+        // C# app directory which can make the game crash.  This must be done before CreateProcess.
+        // This also changes the working directory, which will break Deviare's ability to find
+        // the NativePlugin, so we'll use full path descriptions for the DLL load.
+
+        Directory.SetCurrentDirectory(Path.GetDirectoryName(game));
+
+        // Launch the game, but suspended, so we can hook our first call and be certain to catch it.
+
+        print("Launching: " + game + "...");
+        _gameProcess = _spyMgr.CreateProcess(game, true, out continueevent);
+        if (_gameProcess == null)
+            throw new Exception("Game launch failed.");
+
+        // Ready to go.  Let the game startup.  When it calls Direct3DCreate9, we'll be
+        // called in the NativePlugin::OnFunctionCall
+
+        print("Continue game launch...");
+        _spyMgr.ResumeProcess(_gameProcess, continueevent);
+
+        print("Restoring Working Directory to: " + drawSBS_directory);
+        Directory.SetCurrentDirectory(drawSBS_directory);
     }
 
     // Update is called once per frame
