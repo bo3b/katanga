474c7861fb63a790ef4db29ec9b75059c17547ca
bo3b <bitbucket@bo3b.net>
2017-09-01 00:13:09 -0700

    Drawing test.
    This removes game launch, and updates the drawing code for testing.
    
    The plasma style effect is applied in C# here to draw the Texture2D for the Quad.
    
    This is running and works in VR, but is slowish and low res because of the SetPixel use.
    
    Took forever to figure out that Colors are 0..1, not 0..255

--- Assets/DrawSBS.cs
+++ Assets/DrawSBS.cs
@@ -11,12 +11,35 @@ public class DrawSBS : MonoBehaviour
 {
     static NktSpyMgr _spyMgr;
     static NktProcess _gameProcess;
-    public int width = 1024, height = 1024;
     public Texture2D noiseTex;
 
     // Use this for initialization
     void Start()
     {
+
+        // Create a texture
+        noiseTex = new Texture2D(128, 128, TextureFormat.RGBA32, false);
+        // Set point filtering just so we can see the pixels clearly
+        noiseTex.filterMode = FilterMode.Point;
+
+        
+        // Call Apply() so it's actually uploaded to the GPU
+        for (int y = 0; y < noiseTex.height; y++)
+        {
+            for (int x = 0; x < noiseTex.width; x++)
+            {
+                Color color = ((x & y) != 0 ? Color.white : Color.grey);
+                noiseTex.SetPixel(x, y, color);
+            }
+        }
+        noiseTex.Apply();
+
+        // Set texture onto our material
+        Renderer render = GetComponent<Renderer>();
+        render.material.mainTexture = noiseTex;
+
+        print("applied tex");
+
         int hresult;
         object continueevent;
         string drawSBS_directory = Environment.CurrentDirectory;
@@ -38,59 +61,91 @@ public class DrawSBS : MonoBehaviour
         // This must be reset back to the Unity game directory, otherwise Unity will
         // crash with a fatal error.
 
-        Directory.SetCurrentDirectory(Path.GetDirectoryName(game));
-        {
-            // Launch the game, but suspended, so we can hook our first call and be certain to catch it.
+        //Directory.SetCurrentDirectory(Path.GetDirectoryName(game));
+        //{
+        //    // Launch the game, but suspended, so we can hook our first call and be certain to catch it.
 
-            print("Launching: " + game + "...");
-            _gameProcess = _spyMgr.CreateProcess(game, true, out continueevent);
-            if (_gameProcess == null)
-                throw new Exception("Game launch failed.");
+        //    print("Launching: " + game + "...");
+        //    _gameProcess = _spyMgr.CreateProcess(game, true, out continueevent);
+        //    if (_gameProcess == null)
+        //        throw new Exception("Game launch failed.");
 
-            // Load the NativePlugin for the C++ side.  The NativePlugin must be in this app folder.
-            // The Agent supports the use of Deviare in the CustomDLL, but does not respond to hooks.
+        //    // Load the NativePlugin for the C++ side.  The NativePlugin must be in this app folder.
+        //    // The Agent supports the use of Deviare in the CustomDLL, but does not respond to hooks.
 
-            print("Load NativePlugin... " + nativeDLLName);
-            _spyMgr.LoadAgent(_gameProcess);
-            int result = _spyMgr.LoadCustomDll(_gameProcess, nativeDLLName, true, true);
-            if (result < 0)
-                throw new Exception("Could not load NativePlugin DLL.");
-            
-            // Hook the primary DX9 creation call of Direct3DCreate9, which is a direct export of 
-            // the d3d9 DLL.  All DX9 games must call this interface.
-            // We set this to flgOnlyPostCall, because we want to use the IDirect3D9 object it returns.
+        //    print("Load NativePlugin... " + nativeDLLName);
+        //    _spyMgr.LoadAgent(_gameProcess);
+        //    int result = _spyMgr.LoadCustomDll(_gameProcess, nativeDLLName, true, true);
+        //    if (result < 0)
+        //        throw new Exception("Could not load NativePlugin DLL.");
 
-            print("Hook the D3D9.DLL!Direct3DCreate9...");
-            NktHook d3dHook = _spyMgr.CreateHook("D3D9.DLL!Direct3DCreate9", (int)eNktHookFlags.flgOnlyPostCall);
-            if (d3dHook == null)
-                throw new Exception("Failed to hook D3D9.DLL!Direct3DCreate9");
+        //    // Hook the primary DX9 creation call of Direct3DCreate9, which is a direct export of 
+        //    // the d3d9 DLL.  All DX9 games must call this interface.
+        //    // We set this to flgOnlyPostCall, because we want to use the IDirect3D9 object it returns.
 
-            // Make sure the CustomHandler in the NativePlugin at OnFunctionCall gets called when this 
-            // object is created. At that point, the native code will take over.
+        //    print("Hook the D3D9.DLL!Direct3DCreate9...");
+        //    NktHook d3dHook = _spyMgr.CreateHook("D3D9.DLL!Direct3DCreate9", (int)eNktHookFlags.flgOnlyPostCall);
+        //    if (d3dHook == null)
+        //        throw new Exception("Failed to hook D3D9.DLL!Direct3DCreate9");
 
-            d3dHook.AddCustomHandler(nativeDLLName, 0, "");
+        //    // Make sure the CustomHandler in the NativePlugin at OnFunctionCall gets called when this 
+        //    // object is created. At that point, the native code will take over.
 
-            // Finally attach and activate the hook in the still suspended game process.
+        //    d3dHook.AddCustomHandler(nativeDLLName, 0, "");
 
-            d3dHook.Attach(_gameProcess, true);
-            d3dHook.Hook(true);
+        //    // Finally attach and activate the hook in the still suspended game process.
+
+        //    d3dHook.Attach(_gameProcess, true);
+        //    d3dHook.Hook(true);
 
 
-            // Ready to go.  Let the game startup.  When it calls Direct3DCreate9, we'll be
-            // called in the NativePlugin::OnFunctionCall
+        //    // Ready to go.  Let the game startup.  When it calls Direct3DCreate9, we'll be
+        //    // called in the NativePlugin::OnFunctionCall
 
-            print("Continue game launch...");
-            _spyMgr.ResumeProcess(_gameProcess, continueevent);
-        }
+        //    print("Continue game launch...");
+        //    _spyMgr.ResumeProcess(_gameProcess, continueevent);
+        //}
 
-        print("Restoring Working Directory to: " + drawSBS_directory);
-        Directory.SetCurrentDirectory(drawSBS_directory);
+        //print("Restoring Working Directory to: " + drawSBS_directory);
+        //Directory.SetCurrentDirectory(drawSBS_directory);
     }
 
+
+
+    void ModifyTexturePixels()
+    {
+        int width = noiseTex.width;
+        int height = noiseTex.height;
+
+        float t = Time.timeSinceLevelLoad * 4.0f;
+        Color col = new Color();
+
+        for (int y = 0; y < height; ++y)
+        {
+            for (int x = 0; x < width; ++x)
+            {
+                // Simple "plasma effect": several combined sine waves
+                float vv = (float)(
+                    (127.0f + (127.0f * Math.Sin(x / 7.0f + t))) +
+                    (127.0f + (127.0f * Math.Sin(y / 5.0f - t))) +
+                    (127.0f + (127.0f * Math.Sin((x + y) / 6.0f - t))) +
+                    (127.0f + (127.0f * Math.Sin(Math.Sqrt((x * x + y * y)) / 4.0f - t)))
+                    ) / 4 / 256;
+
+                // Write the texture pixel
+                col[0] = vv;
+                col[1] = vv;
+                col[2] = vv;
+                col[3] = vv;
+                noiseTex.SetPixel(x, y, col);
+            }
+        }
+        noiseTex.Apply();
+    }
 
     // Update is called once per frame
     void Update()
     {
-
+        ModifyTexturePixels();
     }
 }
