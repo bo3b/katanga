0232c45bfc5a364c20a07629dbe156dabdb8f7eb
Bo3b Johnson <bitbucket@bo3b.net>
2018-08-11 21:46:05 -0700

    Create ReleaseBuild script.
    This is setup as a menu option in the Unity editor, so that a build can be created at any time, without needing to use the normal Build menu manually.
    
    Scripted.  Creates the full Release build, and cleanly deletes prior one.
    
    This also allows copying needed files to the output, like the deviare32/64.db files for SpyMgr.Init to succeed.

---
+++ UnityScreenApp/Assets/Editor/ReleaseBuild.cs
@@ -0,0 +1,28 @@
+﻿
+
+using UnityEditor;
+using UnityEngine;
+
+public class ReleaseBuild : MonoBehaviour
+{
+    [MenuItem("Build/Release Build _F5")]
+    public static void Build()
+    {
+        string releaseFolder = "../Release/";
+
+        // Always clean build, delete the prior build completely.
+        FileUtil.DeleteFileOrDirectory(releaseFolder);
+
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Movie.unity" };
+        buildPlayerOptions.locationPathName = releaseFolder + "katanga.exe";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+        buildPlayerOptions.options = BuildOptions.None;
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+
+        // Copy the key Deviare pieces to the output folder, as the
+        // Plugin folder by itself is not sufficient for SpyMgr.Init to succeed.
+        FileUtil.CopyFileOrDirectory("Assets/Dependencies/deviare32.db", releaseFolder + "katanga_data/Plugins/deviare32.db");
+        FileUtil.CopyFileOrDirectory("Assets/Dependencies/deviare64.db", releaseFolder + "katanga_data/Plugins/deviare64.db");
+    }
+}
