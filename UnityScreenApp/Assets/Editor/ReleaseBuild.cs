05dd3e2927ea1e391c36bb12a2f10951f4ef642c
Bo3b Johnson <bitbucket@bo3b.net>
2018-09-03 23:09:44 -0700

    Update build menu to add Debug build.
    Same, based on Editor running, but builds for Debug instead of Release.  Adds second menu item.

--- UnityScreenApp/Assets/Editor/ReleaseBuild.cs
+++ UnityScreenApp/Assets/Editor/ReleaseBuild.cs
@@ -24,5 +24,30 @@ public class ReleaseBuild : MonoBehaviour
         // Plugin folder by itself is not sufficient for SpyMgr.Init to succeed.
         FileUtil.CopyFileOrDirectory("Assets/Dependencies/deviare32.db", releaseFolder + "katanga_data/Plugins/deviare32.db");
         FileUtil.CopyFileOrDirectory("Assets/Dependencies/deviare64.db", releaseFolder + "katanga_data/Plugins/deviare64.db");
+
+        FileUtil.CopyFileOrDirectory("Assets/Dependencies/katanga.exe.manifest", releaseFolder + "katanga_data/Plugins/katanga.exe.manifest");
+    }
+
+    [MenuItem("Build/Debug Build")]
+    public static void DebugBuild()
+    {
+        string releaseFolder = "../Debug/";
+
+        // Always clean build, delete the prior build completely.
+        FileUtil.DeleteFileOrDirectory(releaseFolder);
+
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Movie.unity" };
+        buildPlayerOptions.locationPathName = releaseFolder + "katanga.exe";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+        buildPlayerOptions.options = BuildOptions.Development | BuildOptions.ShowBuiltPlayer;
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+
+        // Copy the key Deviare pieces to the output folder, as the
+        // Plugin folder by itself is not sufficient for SpyMgr.Init to succeed.
+        FileUtil.CopyFileOrDirectory("Assets/Dependencies/deviare32.db", releaseFolder + "katanga_data/Plugins/deviare32.db");
+        FileUtil.CopyFileOrDirectory("Assets/Dependencies/deviare64.db", releaseFolder + "katanga_data/Plugins/deviare64.db");
+
+        FileUtil.CopyFileOrDirectory("Assets/Dependencies/katanga.exe.manifest", releaseFolder + "katanga_data/Plugins/katanga.exe.manifest");
     }
 }
